ea5e741fe9afab619b24504bc5edebb6f9f345f8
lsyf <linlsyf@gmail.com>
2018-05-21 11:30:16 +0800

    修改进度条背景

---
+++ lib/src/main/java/com/easysoft/widget/base/IBaseView.java
@@ -0,0 +1,11 @@
+package com.easysoft.widget.base;
+
+import android.content.Context;
+
+/**
+ * Created by lindanghong on 2018/2/7.
+ */
+public interface IBaseView {
+    Context getContext();
+    void showToast(String text);
+}
